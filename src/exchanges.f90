d5800ca66fbbcb5872ae51bd787c01829fbf2c1e
vsrsousa <vsrsousa@users.noreply.github.com>
2026-06-13 01:21:58 -0300

    refactor: add setup_mod::setup_system and delegate system reading/printing

--- src/exchanges.f90
+++ src/exchanges.f90
@@ -86,66 +86,7 @@ program exchange_parameters
   write(stdout,'(5x,a7,f7.3,a9,f6.3,a11,f5.3,a5)') 'emin = ', emin, '  emax = ', emax, '  height = ', height,' (eV)'
   write(stdout,'(5x,a6,i4,a8,i4,a8,i4)') 'nz1 = ', nz1, '  nz2 = ', nz2, '  nz3 = ', nz3
 
-  call read_hamilt()
-  call read_crystal()
-  ! read_hamilt moved to hamiltonian_mod
-
-  ! report memory after reading input H and crystal
-  call print_mem_status('After reading inputs')
-
-  if(atom_of_interest == -100) atom_of_interest = block_atom(1)
-
-  ! debug
-  if( iverbosity .ge. 3) then
-    ! output H(0)
-    allocate( hksum(hdim,hdim,nspin) )
-    hksum = cmplx(0.0,0.0,dp)
-
-    do i=1, nkp
-      hksum(:,:,:) = hksum(:,:,:) + h(:,:,i,:)*wk(i)
-    end do
-
-    do i=1, nspin
-      write(stdout,'(/,5x,a13,i2,a1)') 'H(0) for spin', i, ':'
-      call output_matrix_by_blocks(hdim,dreal(hksum(:,:,i)))
-    end do
-
-    deallocate( hksum ) 
-  end if
-  ! end of debug
-  
-  ! Output of the readed values
-  write(stdout,'(/,5x,a20,f13.9,a5)') 'Cell constant (alat):', alat, 'Bohr'
-  write(stdout,'(5x,20a)') 'Cell vectors (rows):'
-  do i = 1, 3
-    write(stdout,'(7x,3f9.5)') cell(:,i)
-  end do
-  write(stdout,'(5x,a26)') 'Atoms of the initial cell:'
-  do i = 1, natoms
-    write(stdout,'(7x,a3,x,3f9.5)') atomlabel(i), tau(:,i)
-  end do
-  
-  write(stdout,'(/,5x,a71)') 'We have the following basis for the hamiltonian and the green function:'
-  write(stdout,'(5x,i3,a20,i3,a8)') hdim, ' orbitals grouped in', nblocks, ' blocks:'
-  do i=1, nblocks
-    write(fmt,'(i1)') block_dim(i)
-    write(stdout,'(5x,a3,a7,i2,a2,3x,i1,x,a,a11,'//adjustl(fmt)//'a11,a1)') &
-        adjustr(atomlabel(block_atom(i))), ' (atom ', block_atom(i), '):', &
-        block_dim(i),block_l(i), '-orbitals (', (orbitals(block_orbitals(i,j)), j=1, block_dim(i)), ')'
-  end do
-  
-  call atoms_list(mode,distance,atom_of_interest,l)
-
-  ! Pretty output
-  write(stdout,'(/5x,a17,i4,a8)') 'We will consider ', nnnbrs,' atoms: '
-
-  do i = 1, nnnbrs
-    pos_delta = sqrt( (taunew(1,i)-tau(1,atom_of_interest))**2 + &
-                  (taunew(2,i)-tau(2,atom_of_interest))**2 + &
-                  (taunew(3,i)-tau(3,atom_of_interest))**2 )
-    write(stdout,'(5x,i2,a2,a3,x,3f9.5,3x,a12,f9.5,a7)') i, ': ', &
-      atomlabel( block_atom( parent(i) ) ), taunew(:,i), '( distance =', pos_delta, ' alat )'
-  end do
+  call setup_system(mode, distance, l, atom_of_interest)
 
 
 
